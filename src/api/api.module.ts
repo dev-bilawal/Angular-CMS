4744bb719c246e2a03244b70f335c56309097057
M-Usman45 <usman@visualr.com.au>
2023-05-19 20:53:31 +0500

    add

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -8,3 +8,31 @@ import { CustomersService } from './services/customers.service';
 import { PrintLabsService } from './services/printlabs.service';
 import { RealmService } from './services/realm.service';
 import { UserService } from './services/user.service';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+  ]
+})
+export class ApiModule {
+  static forRoot(): ModuleWithProviders<ApiModule> {
+    return {
+      ngModule: ApiModule,
+      providers: [
+        RealmService,
+        CountriesService,
+        CustomersService,
+        UserService,
+        CompaniesService,
+        PrintLabsService,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthorizationInterceptor,
+          multi: true
+        },
+      ]
+    };
+  }
+}
